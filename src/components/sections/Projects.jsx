cc6cbee36482a493e67e0318b30542b8ed9323e3
FabienThich <fabienthich30@gmail.com>
2025-09-22 20:34:11 -0400

    add correct tech stack, grow cols for projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,24 +14,32 @@ export const Projects = () => {
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {/* 1st Box */}
-            <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition flex flex-col h-full">
               <h3 className="text-xl font-bold mb-2">Amazon Prime Reviews</h3>
               <p className="text-gray-400 mb-4">
                 Analyze Amazon Prime video reviews with Natural Language
                 Processing.
               </p>
               <div className="flex flex-wrap gap-2 mb-4 cursor-pointer">
-                {["NumPy", "Pandas", "NLTK", "Jupyter Notebook"].map(
-                  (tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
-                    >
-                      {tech}
-                    </span>
-                  )
-                )}
+                {[
+                  "Pandas",
+                  "NumPy",
+                  "Seaborn",
+                  "Matplotlib",
+                  "Statsmodel",
+                  "SciPy",
+                  "NLTK",
+                  "WordCloud",
+                ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
               </div>
+              <div className="flex-grow" />
               <div className="flex justify-between items-center">
                 <a
                   href="https://github.com/FabienThich/Math-1130-A3"
@@ -44,7 +52,7 @@ export const Projects = () => {
             </div>
 
             {/* 2nd Box */}
-            <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition flex flex-col h-full">
               <h3 className="text-xl font-bold mb-2">Overwatch</h3>
               <p className="text-gray-400 mb-4">
                 Data-driven insights on win rates and gameplay dynamics of teams
@@ -62,6 +70,7 @@ export const Projects = () => {
                   )
                 )}
               </div>
+              <div className="flex-grow" />
               <div className="flex justify-between items-center">
                 <a
                   href="#"
@@ -74,7 +83,7 @@ export const Projects = () => {
             </div>
 
             {/* 3rd Box */}
-            <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+            <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition flex flex-col h-full">
               <h3 className="text-xl font-bold mb-2">
                 Global Air Pollution Analysis
               </h3>
@@ -83,7 +92,7 @@ export const Projects = () => {
                 understand their impact on public health and the environment.
               </p>
               <div className="flex flex-wrap gap-2 mb-4 cursor-pointer">
-                {["NumPy", "Pandas", "NLTK", "Jupyter Notebook"].map(
+                {["Pandas", "NumPy", "Seaborn", "Matplotlib", "Statsmodel"].map(
                   (tech, key) => (
                     <span
                       key={key}
@@ -94,6 +103,7 @@ export const Projects = () => {
                   )
                 )}
               </div>
+              <div className="flex-grow" />
               <div className="flex justify-between items-center">
                 <a
                   href="https://github.com/FabienThich/Math-1130-A2"
